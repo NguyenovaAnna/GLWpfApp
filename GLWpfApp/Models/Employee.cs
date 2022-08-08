5f034de40b5d54c71f81dd8a73d90a818f2d7878
Anna Nguyenova <nguyenova.anna@gmail.com>
2022-08-08 13:34:52 +0200

    Add project files.

---
+++ GLWpfApp/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GLWpfApp.Models
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+
+        }
+    }
+}
