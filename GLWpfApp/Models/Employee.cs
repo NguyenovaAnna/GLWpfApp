4012b8a36d77afa63e6600af3fa44d90ebff7ffd
Anna Nguyenova <nguyenova.anna@gmail.com>
2022-08-10 13:45:07 +0200

    Added MVVM pattern

--- GLWpfApp/Models/Employee.cs
+++ GLWpfApp/Models/Employee.cs
@@ -1,16 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GLWpfApp.Models
 {
-    public class Employee
+    public class Employee : INotifyPropertyChanged
     {
-        public string FirstName { get; set; }
+        private string _firstName;
+        private string _lastName;
+        private string _fullname;
 
-        public string LastName { get; set; }
+        public string FirstName
+        {
+            get 
+            { 
+                return _firstName; 
+            }
+            set 
+            { 
+                _firstName = value;
+                OnPropertyChanged(FirstName);
+            }
+        }
+
+        
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged(LastName);
+            }
+        }
+
 
         public string FullName
         {
@@ -18,7 +47,15 @@ namespace GLWpfApp.Models
             {
                 return $"{FirstName} {LastName}";
             }
+        }
 
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string p)
+        {
+            PropertyChangedEventHandler ph = PropertyChanged;
+            if (ph != null)
+                ph(this, new PropertyChangedEventArgs(p));
         }
     }
 }
---
+++ GLWpfApp/ViewModels/EmployeeListViewModel.cs
@@ -0,0 +1,83 @@
+﻿using GLWpfApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace GLWpfApp.ViewModels
+{
+    public class EmployeeListViewModel : INotifyPropertyChanged
+    {
+
+        private ObservableCollection<Employee> _employees;
+        public ObservableCollection<Employee> Employees
+        {
+            get
+            {
+                return _employees;
+            }
+            set
+            {
+                _employees = value;
+                OnPropertyChanged(nameof(Employees));
+            }
+        }
+
+        public ICollectionView EmployeesCollectionView { get; }
+
+        private string _employeesFilter = string.Empty;
+        public string EmployeesFilter
+        {
+            get
+            {
+                return _employeesFilter;
+            }
+            set
+            {
+                _employeesFilter = value;
+                OnPropertyChanged(nameof(EmployeesFilter));
+                EmployeesCollectionView.Refresh();
+            }
+        }
+
+        public EmployeeListViewModel()
+        {
+            Employees = new ObservableCollection<Employee>()
+            {
+                new Employee { FirstName = "Anna", LastName = "Nguyenova" },
+                new Employee { FirstName = "Daniela", LastName = "Horvathova" },
+                new Employee { FirstName = "Dominika", LastName = "Mala" },
+                new Employee { FirstName = "David", LastName = "Kovac" },
+                new Employee { FirstName = "Peter", LastName = "Duris" }
+            };
+
+            EmployeesCollectionView = CollectionViewSource.GetDefaultView(Employees);
+            EmployeesCollectionView.Filter = FilterEmployees;
+        }
+
+        private bool FilterEmployees(object obj)
+        {
+            if(obj is Employee Employee)
+            {
+                return Employee.FullName.Contains(EmployeesFilter, StringComparison.InvariantCultureIgnoreCase);
+            }
+
+            return false;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
